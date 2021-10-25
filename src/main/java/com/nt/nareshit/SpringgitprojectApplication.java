db33d4d4683cf610cd189d17fb8308213e77ecbf
abc <abc@example.com>
2021-10-25 09:26:52 +0530

    inital commit

---
+++ src/main/java/com/nt/nareshit/SpringgitprojectApplication.java
@@ -0,0 +1,13 @@
+package com.nt.nareshit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgitprojectApplication.class, args);
+	}
+
+}
